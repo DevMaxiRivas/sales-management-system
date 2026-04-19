8485b5edff2f0e5227c11fdf611d90205cfe589e
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-04-19 15:24:03 -0300

    read prices feature in InvoiceOCRService have been implemented

--- app/Services/Invoice/InvoiceOcrService.php
+++ app/Services/Invoice/InvoiceOcrService.php
@@ -11,7 +11,7 @@ class InvoiceOcrService
 {
     public function __construct(private OcrReaderInterface $ocrReader) {}
 
-    public function extractLinesFromInvoiceImage(string $path, ?string $language = 'spa'): array
+    protected function extractLinesFromInvoiceImage(string $path, ?string $language = 'spa'): array
     {
         try {
             $text = $this->ocrReader->extractText($path, $language);
@@ -29,40 +29,92 @@ class InvoiceOcrService
         }
     }
 
-    public function extractProductIdsFromInvoiceImage(string $path, ?string $language = 'spa', bool $ids_are_numeric = false, ?string $pattern = null): array
+    protected function filterDataByPattern(array $data, string $pattern): array
+    {
+        return
+            array_values(
+                array_filter(
+                    array: array_map(
+                        callback: function (string $element) use ($pattern) {
+                            // preg_grep throws an exception if a pattern with incorrect syntax is provided
+                            if (count(preg_grep($pattern, [$element])) == 0) {
+                                return null;
+                            }
+
+                            return preg_replace('/[^0-9]/', '', $element);
+                        },
+                        array: $data
+                    ),
+                    callback: fn($element) => !is_null($element)
+                )
+            );
+    }
+
+    protected function filterPricesByPattern(array $data, ?string $pattern = null): array
+    {
+        if (is_null($pattern)) $pattern = '/[\d]+(\d{2})/';
+        return
+            array_values(
+                array_filter(
+                    array: array_map(
+                        callback: function (string $element) use ($pattern) {
+                            if (preg_match('/\d+(?:[.,]\d+)*/', $element, $matches)) {
+                                $numWithoutDecimal = preg_replace('/[^0-9]/', '', $matches[0]);
+
+                                preg_match($pattern, $numWithoutDecimal, $matches2);
+                                $decimal = $matches2[1] ?? '';
+                                $lenDecimal = strlen($decimal);
+
+                                return floatval(substr($numWithoutDecimal, 0, -$lenDecimal) . '.' . $decimal);
+                            }
+
+                            return null;
+                        },
+                        array: $data
+                    ),
+                    callback: fn($element) => !is_null($element)
+                )
+            );
+    }
+
+    protected function extractDataWithoutSpaces(string $path, ?string $language = 'spa'): array
     {
         $data = array_map(
             fn(string $line) => $this->normalizeInvoiceLine($line),
             $this->extractLinesFromInvoiceImage($path, $language)
         );
 
-        $data = array_map(fn(string $element) => str_replace(' ', '', $element), $data);
+        return array_map(fn(string $element) => str_replace(' ', '', $element), $data);
+    }
+
+    public function extractProductIdsFromInvoiceImage(string $path, ?string $language = 'spa', bool $ids_are_numeric = false, ?string $pattern = null): array
+    {
+        $data = $this->extractDataWithoutSpaces($path, $language);
 
         if ($ids_are_numeric) {
-            try {
-                return
-                    array_values(
-                        array_filter(
-                            array: array_map(
-                                callback: function (string $element) use ($pattern) {
-                                    // preg_grep throws an exception if a pattern with incorrect syntax is provided
-                                    if (!is_null($pattern) && count(preg_grep($pattern, [$element])) == 0) {
-                                        return null;
-                                    }
-
-                                    return preg_replace('/[^0-9]/', '', $element);
-                                },
-                                array: $data
-                            ),
-                            callback: fn($element) => !is_null($element)
-                        )
-                    );
-            } catch (\Throwable $th) {
-                Log::info($th->getMessage());
-                return [];
+            if (!is_null($pattern)) {
+                try {
+                    return $this->filterDataByPattern($data, $pattern);
+                } catch (\Throwable $th) {
+                    Log::info($th->getMessage());
+                    return [];
+                }
+            } else {
+                return array_map(fn(string $element) => preg_replace('/[^0-9]/', '', $element), $data);
             }
         }
         return $data;
+    }
+
+    public function extractPricesFromInvoiceImage(string $path, ?string $language = 'spa', ?string $pattern = null): array
+    {
+        $data = $this->extractDataWithoutSpaces($path, $language);
+        try {
+            return $this->filterPricesByPattern($data, $pattern);
+        } catch (\Throwable $th) {
+            Log::info($th->getMessage());
+            return [];
+        }
     }
 
     private function normalizeInvoiceLine(string $line): string
